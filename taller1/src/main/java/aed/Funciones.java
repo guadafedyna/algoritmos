091e7316f672f3e6c43ae3922b01804fa3f3d3fc
Guadalupe Fedyna <guadafedyna@gmail.com>
2026-08-20 00:58:51 -0300

    completo

--- taller1/src/main/java/aed/Funciones.java
+++ taller1/src/main/java/aed/Funciones.java
@@ -41,51 +41,104 @@ class Funciones {
     }
 
     boolean esPrimo(int n) {
-        // COMPLETAR
-        return false;
+        int contadorDivisores = 0;
+        for (int i= 1; i<= n; i++){
+            if (esDivisor(i,n)){
+                contadorDivisores ++;
+            }
+        }
+        return contadorDivisores == 2;
+    }
+
+    boolean esDivisor(int divisor, int n){
+        return n % divisor == 0;
     }
 
     int sumatoria(int[] numeros) {
-        // COMPLETAR
-        return 0;
+        int longitud = numeros.length;
+        int res = 0;
+
+        for (int i=0; i < longitud; i++){
+            res += numeros[i];
+        }
+        return res;
     }
 
     int busqueda(int[] numeros, int buscado) {
-        // COMPLETAR
-        return 0;
+        int longitud = numeros.length;
+        int posicion = 0;
+
+        for(int i=0; i<longitud; i++){
+            if (numeros[i] == buscado){
+                posicion = i;
+            }
+        }
+        return posicion;
     }
 
     boolean tienePrimo(int[] numeros) {
-        // COMPLETAR
+        int longitud = numeros.length;
+
+        for(int i = 0; i< longitud; i++){
+            if (esPrimo(numeros[i])){
+                return true;
+            }
+        }
         return false;
     }
 
     boolean todosPares(int[] numeros) {
-        // COMPLETAR
-        return false;
+        int longitud = numeros.length;
+
+        for(int i=0; i<longitud; i++){
+            if (!(esPar(numeros[i]))){
+                return false;
+            }
+        }
+        return true;
     }
 
     boolean esPrefijo(String s1, String s2) {
-        // COMPLETAR
-        return false;
+        int longitud1 = s1.length(); 
+        int longitud2 = s2.length();
+
+        for(int i = 0; i < longitud1; i++){
+            if ((s1.charAt(i) != s2.charAt(i)) | (longitud1 > longitud2) ){
+                return false;
+            }
+        }
+        return true;
     }
 
     boolean esSufijo(String s1, String s2) {
-        // COMPLETAR
-        return false;
+        int longitud1 = s1.length();
+        int longitud2 = s2.length();
+
+        for(int i=0; i< longitud1; i++){
+            if((s1.charAt(longitud1-i-1) != s2.charAt(longitud2-i-1)) | longitud1 > longitud2){
+                return false;
+            }
+        }
+        return true;
     }
 
 /***  Segunda parte: Debugging ***/
 
     boolean xor(boolean a, boolean b) {
-        return a || b && !(a && b);
+        return (a || b) && (!(a && b));
     }
 
     boolean iguales(int[] xs, int[] ys) {
         boolean res = true;
+        int longitudx = xs.length;
+        int longitudy = ys.length;
+
+        if (longitudx != longitudy){
+            return false;
+        }
 
         for (int i = 0; i < xs.length; i++) {
-            if (xs[i] != ys[i]) {
+            if ((xs[i] != ys[i]))  {
                 res = false;
             }
         }
@@ -94,8 +147,8 @@ class Funciones {
 
     boolean ordenado(int[] xs) {
         boolean res = true;
-        for (int i = 0; i < xs.length; i++) {
-            if (xs[i] > xs [i+1]) {
+        for (int i = 0; i < xs.length-1 ; i++) {
+            if (xs[i] > xs[i+1]) {
                 res = false;
             }
         }
@@ -103,21 +156,21 @@ class Funciones {
     }
 
     int maximo(int[] xs) {
-        int res = 0;
-        for (int i = 0; i <= xs.length; i++) {
-            if (xs[i] > res) res = i;
+        int res = xs[0];
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] > res){
+                res = xs[i];
+            }
         }
         return res;
     }
 
     boolean todosPositivos(int[] xs) {
-        boolean res = false;
+        boolean res = true;
         for (int x : xs) {
-            if (x > 0) {
-                res = true;
-            } else {
+            if (x <= 0) {
                 res = false;
-            }
+            } 
         }
         return res;
     }
